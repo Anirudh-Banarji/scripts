9230e1227156d4f6543516531dbbebc526b91f00
Anirudh Banarji <anirudhbanarji@gmail.com>
2013-07-10 04:53:01 +0000

    Modified name of script and status messages

--- control_leofs.sh
+++ control_leofs.sh
@@ -8,60 +8,82 @@ case "$1" in
         		0)
 				case "$3" in
 					start)
+						printf "\nStarting manager 0:"
 						/usr/local/leofs/package/leo_manager_0/bin/leo_manager start
 					;;
 					stop)
+						printf "\nStopping manager 0:"
 						/usr/local/leofs/package/leo_manager_0/bin/leo_manager stop
 					;;
+					*)printf "$USAGE"
 				esac
 				;;	
 			1)	case "$3" in
 					start)
+						printf "\nStarting manager 1:"
 						/usr/local/leofs/package/leo_manager_1/bin/leo_manager start
 					;;
 					stop)
+						printf "\nStopping manager 1:"
 						/usr/local/leofs/package/leo_manager_1/bin/leo_manager stop
 					;;
+					*)printf "$USAGE"
 				esac
 				;;
+				*)printf "$USAGE"
 		esac
 		;;
 	storage)
 		case "$2" in
         		start)
+				printf "\nStarting storage:"
 				/usr/local/leofs/package/leo_storage/bin/leo_storage start
 			;;
 			stop)
+				printf "\nStopping storage:"
 				/usr/local/leofs/package/leo_storage/bin/leo_storage stop
 			;;
+			*)printf "$USAGE"
 		esac
 		;;
 	gateway)
 		case "$2" in
         		start)
+				printf "\nStarting gateway:"
 				/usr/local/leofs/package/leo_gateway/bin/leo_gateway start
 			;;
 			stop)
+				printf "\nStopping gateway:"
 				/usr/local/leofs/package/leo_gateway/bin/leo_gateway stop
 			;;
+			*)printf "$USAGE"
 		esac
 		;;
 	all)
 		case "$2" in
 			start)
+				printf "\nStarting manager 0:"
 				/usr/local/leofs/package/leo_manager_0/bin/leo_manager start
+				printf "\nStarting manager 1:"
 				/usr/local/leofs/package/leo_manager_1/bin/leo_manager start
+				printf "\nStarting storage:"
 				/usr/local/leofs/package/leo_storage/bin/leo_storage start
+				printf "\nStarting gateway:"
 				/usr/local/leofs/package/leo_gateway/bin/leo_gateway start
 
 			;;
 			stop)
+				printf "\nStopping manager 0:"
 				/usr/local/leofs/package/leo_manager_0/bin/leo_manager stop
+				printf "\nStopping manager 1:"
 				/usr/local/leofs/package/leo_manager_1/bin/leo_manager stop
+				printf "\nStopping storage:"
 				/usr/local/leofs/package/leo_storage/bin/leo_storage stop
+				printf "\nStopping gateway:"
 				/usr/local/leofs/package/leo_gateway/bin/leo_gateway stop
 			
 			;;
+			*)printf "$USAGE"
 		esac
 		;;	
 	*)printf "$USAGE"
